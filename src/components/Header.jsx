d7aa3711823f080003103119654b16378e4b168e
cam96stanley <cam96stanley@gmail.com>
2024-11-24 23:37:33 -0500

    Added logic to close menu when clicking outside menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,32 @@
 import { Link, NavLink } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function Header() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
+  const sidebarRef = useRef(null);
+
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
+
+  const activeStyle = {
+    borderBottom: "4px solid #93C5FD",
+  };
+
+  const handleClickOutside = (event) => {
+    if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+      setIsSidebarOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <>
       <div className="flex justify-between items-center p-6">
@@ -17,26 +37,45 @@ export default function Header() {
           <img src="../../public/shared/icon-hamburger.svg" />
         </button>
       </div>
-      <div className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
+      <div
+        ref={sidebarRef}
+        className={`sidebar ${isSidebarOpen ? "open" : ""}`}
+      >
         <ul>
           <li>
-            <NavLink to="/">
-              <span>00</span>HOME
+            <NavLink
+              to="/"
+              className="hover:border-b-2"
+              style={({ isActive }) => (isActive ? activeStyle : null)}
+            >
+              <span className="mr-5 opacity-50">00</span>HOME
             </NavLink>
           </li>
           <li>
-            <NavLink to="destination">
-              <span>01</span>DESTINATION
+            <NavLink
+              to="/destination"
+              className="hover:border-b-2"
+              style={({ isActive }) => (isActive ? activeStyle : null)}
+            >
+              <span className="mr-5 opacity-50">01</span>DESTINATION
             </NavLink>
           </li>
           <li>
-            <NavLink to="crew">
-              <span>02</span>CREW
+            <NavLink
+              to="/crew"
+              className="hover:border-b-2"
+              style={({ isActive }) => (isActive ? activeStyle : null)}
+            >
+              <span className="mr-5 opacity-50">02</span>CREW
             </NavLink>
           </li>
           <li>
-            <NavLink to="technology">
-              <span>03</span>TECHNOLOGY
+            <NavLink
+              to="/technology"
+              className="hover:border-b-2"
+              style={({ isActive }) => (isActive ? activeStyle : null)}
+            >
+              <span className="mr-5 opacity-50">03</span>TECHNOLOGY
             </NavLink>
           </li>
         </ul>
